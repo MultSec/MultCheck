904b8fd70f7ac479bdadf4cafa7533a47a2045f1
Pengrey <rodrigoflima.me@gmail.com>
2024-02-21 14:46:01 +0000

    Added dynamic scanner check

--- src/pkg/scan/scan.go
+++ src/pkg/scan/scan.go
@@ -29,6 +29,48 @@ func scanFile(binaryPath string, conf map[string]string) bool {
 	}
 }
 
+func scanSlice(fileData []byte, conf map[string]string) bool {
+	// Create a temp file to scan
+	tempFile, err := os.CreateTemp("", "slice_scan_")
+	if err != nil {
+		fmt.Println("[!] Error creating temp file:", err)
+		os.Exit(1)
+	}
+
+	// Defer cleanup
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	_, err = tempFile.Write(fileData)
+	if err != nil {
+		fmt.Println("[!] Error writing to temp file:", err)
+		os.Exit(1)
+	}
+
+	// Scan the file slice
+	return scanFile(tempFile.Name(), conf)
+}
+
+func checkDinamic(binaryPath string, conf map[string]string) bool {
+	// Read the files content
+	data, err := os.ReadFile(binaryPath)
+	if err != nil {
+		fmt.Println("[!] Error reading file:", err)
+		os.Exit(1)
+	}
+
+	// Split the data into half
+	firstHalf := data[:len(data)/2]
+	secondHalf := data[len(data)/2:]
+
+	// Check both halves for malware
+	if scanSlice(firstHalf, conf) || scanSlice(secondHalf, conf) {
+		return true
+	} else {
+		return false
+	}
+}
+
 func CheckMal(binaryPath string, conf map[string]string) string {
 	// Check for Detection
 	if !scanFile(binaryPath, conf) {
@@ -36,6 +78,9 @@ func CheckMal(binaryPath string, conf map[string]string) string {
 	}
 
 	// Check for Dynamic Detection
+	if checkDinamic(binaryPath, conf) {
+		return "Payload detected dynamically."
+	}
 
 	// Check for Static Detection
 
